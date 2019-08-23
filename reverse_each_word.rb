40b6551b429784ef2f0861752ac466766896e321
Yazgeldi Seyidov <yazgeldi.seyidov@gmail.com>
2019-08-23 15:30:37 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 
 def reverse_each_word(sentence)
   array = sentence.split(" ")
@@ -15,4 +16,11 @@ def reverse_each_word(sentence)
       reversed << sentence.reverse
   end
   reversed.join(" ")
+=======
+def reverse_each_word(sentence)
+  array = []
+  sentence.each do |word|
+    array << word.reverse
+end
+>>>>>>> c29f189438b476be73a8d7deb8df64d48bf48a52
 end
